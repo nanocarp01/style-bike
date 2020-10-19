855a93e2c56b7c60500e9448ff7b587d2e646333
nanocarp01 <71789047+nanocarp01@users.noreply.github.com>
2020-10-18 21:27:11 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-/*if (sessionStorage.length == 0) {
-    let nombre = prompt('Ingrese su nombre');
-    sessionStorage.setItem("nombre", nombre);
-}*/
-
-/*while (sessionStorage.length == 0 && nombre.length != null) {
-    ingresar();
-    if (nombre.length == "") {
-        alert('Ingrese su nombre');
-        ingresar();
-    }
-    sessionStorage.setItem("nombre", nombre);
-
-
-}*/
 var nombre = String;
 
 function ingresar(string) {
